91d57e9e2846ba7b15bf3ae7c43b2a63cf2bc5fe
Johannes Schickling <schickling.j@gmail.com>
2015-09-04 10:12:44 +0200

    init xcode

---
+++ ActiveLabel/ActiveLabel.h
@@ -0,0 +1,19 @@
+//
+//  ActiveLabel.h
+//  ActiveLabel
+//
+//  Created by Johannes Schickling on 9/4/15.
+//  Copyright © 2015 Optonaut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ActiveLabel.
+FOUNDATION_EXPORT double ActiveLabelVersionNumber;
+
+//! Project version string for ActiveLabel.
+FOUNDATION_EXPORT const unsigned char ActiveLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ActiveLabel/PublicHeader.h>
+
+
