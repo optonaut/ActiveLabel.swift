e2c7402a236c14e55e03563a2d089fae14c8b28f
Johannes Schickling <schickling.j@gmail.com>
2015-09-04 10:15:43 +0200

    init demo

---
+++ ActiveLabelDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ActiveLabelDemo
+//
+//  Created by Johannes Schickling on 9/4/15.
+//  Copyright © 2015 Optonaut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
