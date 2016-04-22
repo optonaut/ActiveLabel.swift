8ae636c60e697c00c9366aeea27bca3c82c0f3a4
jangwonhee <jangwonhee@jangwonheeui-Mac-Pro.local>
2016-04-23 07:42:57 +0900

    for extense

--- ActiveLabel/RegexParser.swift
+++ ActiveLabel/RegexParser.swift
@@ -10,13 +10,13 @@ import Foundation
 
 struct RegexParser {
     
-    static let urlPattern = "(^|[\\s.:;?\\-\\]<\\(])" +
+    static var urlPattern = "(^|[\\s.:;?\\-\\]<\\(])" +
     "((https?://|www\\.|pic\\.)[-\\w;/?:@&=+$\\|\\_.!~*\\|'()\\[\\]%#,☺]+[\\w/#](\\(\\))?)" +
     "(?=$|[\\s',\\|\\(\\).:;?\\-\\[\\]>\\)])"
     
-    static let hashtagRegex = try? NSRegularExpression(pattern: "(?:^|\\s|$)#[\\p{L}0-9_]*", options: [.CaseInsensitive])
-    static let mentionRegex = try? NSRegularExpression(pattern: "(?:^|\\s|$|[.])@[\\p{L}0-9_]*", options: [.CaseInsensitive]);
-    static let urlDetector = try? NSRegularExpression(pattern: urlPattern, options: [.CaseInsensitive])
+    static var hashtagRegex = try? NSRegularExpression(pattern: "(?:^|\\s|$)#[\\p{L}0-9_]*", options: [.CaseInsensitive])
+    static var mentionRegex = try? NSRegularExpression(pattern: "(?:^|\\s|$|[.])@[\\p{L}0-9_]*", options: [.CaseInsensitive]);
+    static var urlDetector = try? NSRegularExpression(pattern: urlPattern, options: [.CaseInsensitive])
     
     static func getMentions(fromText text: String, range: NSRange) -> [NSTextCheckingResult] {
         guard let mentionRegex = mentionRegex else { return [] }
