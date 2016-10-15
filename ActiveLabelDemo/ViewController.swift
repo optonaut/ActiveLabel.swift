fe6c499d7baf98016ade0f32d96e20482c25ba6d
Andrea Perizzato <andrea.perizzato@gmail.com>
2016-10-15 21:25:48 +0100

    Updated the demo view controller: now you can switch between attributed text and simple text.

--- ActiveLabelDemo/ViewController.swift
+++ ActiveLabelDemo/ViewController.swift
@@ -12,13 +12,17 @@ import ActiveLabel
 class ViewController: UIViewController {
     
     let label = ActiveLabel()
-    let uiLabel = UILabel()
+    let toggleButton = UIButton(type: .system)
+    
+    private var usingAttributedText = false
+    
+    private let defaultText = "This is a post with #multiple #hashtags and a @userhandle. Links are also supported like this one: http://optonaut.co. Now it also supports custom patterns -> are\n\nLet's trim a long link: \nhttps://twitter.com/twicket_app/status/649678392372121601"
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let customType = ActiveType.Custom(pattern: "\\sare\\b") //Looks for "are"
-        let customType2 = ActiveType.Custom(pattern: "\\sit\\b") //Looks for "it"
+        let customType = ActiveType.custom(pattern: "\\sare\\b") //Looks for "are"
+        let customType2 = ActiveType.custom(pattern: "\\sit\\b") //Looks for "it"
 
         label.enabledTypes.append(customType)
         label.enabledTypes.append(customType2)
@@ -27,60 +31,39 @@ class ViewController: UIViewController {
 
         label.customize { label in
             
-            let text = NSMutableAttributedString()
-            
-            let s1 = NSAttributedString(string: "HELLO ", attributes: [
-                NSForegroundColorAttributeName : UIColor.orangeColor(),
-                NSFontAttributeName: UIFont.preferredFontForTextStyle(UIFontTextStyleHeadline).fontWithSize(30)
-            ])
-            let longText = "This is a post with #multiple #hashtags and a @userhandle. Links are also supported like" +
-                " this one: http://optonaut.co. Now it also supports custom patterns -> are\n\n" +
-            "Let's trim a long link: \nhttps://twitter.com/twicket_app/status/649678392372121601"
-            let s2 = NSAttributedString(string: longText, attributes: [
-                NSForegroundColorAttributeName : UIColor(red: 102.0/255, green: 117.0/255, blue: 127.0/255, alpha: 1),
-                NSFontAttributeName: UIFont.preferredFontForTextStyle(UIFontTextStyleBody)
-            ])
-            text.appendAttributedString(s1)
-            text.appendAttributedString(s2)
-
-            label.attributedText = text
-            self.uiLabel.attributedText = NSAttributedString(attributedString: text)
-            self.uiLabel.numberOfLines = 0
-            
-//            label.text = "This is a post with #multiple #hashtags and a @userhandle. Links are also supported like" +
-//            " this one: http://optonaut.co. Now it also supports custom patterns -> are\n\n" +
-//                "Let's trim a long link: \nhttps://twitter.com/twicket_app/status/649678392372121601"
             label.numberOfLines = 0
             label.lineSpacing = 4
             
-//            label.textColor = UIColor(red: 102.0/255, green: 117.0/255, blue: 127.0/255, alpha: 1)
+            label.textColor = UIColor(red: 102.0/255, green: 117.0/255, blue: 127.0/255, alpha: 1)
             label.hashtagColor = UIColor(red: 85.0/255, green: 172.0/255, blue: 238.0/255, alpha: 1)
             label.mentionColor = UIColor(red: 238.0/255, green: 85.0/255, blue: 96.0/255, alpha: 1)
             label.URLColor = UIColor(red: 85.0/255, green: 238.0/255, blue: 151.0/255, alpha: 1)
             label.URLSelectedColor = UIColor(red: 82.0/255, green: 190.0/255, blue: 41.0/255, alpha: 1)
 
-            label.handleMentionTap { self.alert("Mention", message: $0) }
-            label.handleHashtagTap { self.alert("Hashtag", message: $0) }
-            label.handleURLTap { self.alert("URL", message: $0.absoluteString!) }
+            label.handleMentionTap { self.alert(title: "Mention", message: $0) }
+            label.handleHashtagTap { self.alert(title: "Hashtag", message: $0) }
+            label.handleURLTap { self.alert(title: "URL", message: $0.absoluteString) }
 
             //Custom types
 
-            label.customColor[customType] = UIColor.purpleColor()
-            label.customSelectedColor[customType] = UIColor.greenColor()
-            label.customColor[customType2] = UIColor.magentaColor()
-            label.customSelectedColor[customType2] = UIColor.greenColor()
+            label.customColor[customType] = UIColor.purple
+            label.customSelectedColor[customType] = UIColor.green
+            label.customColor[customType2] = UIColor.magenta
+            label.customSelectedColor[customType2] = UIColor.green
 
-            label.handleCustomTap(for: customType) { self.alert("Custom type", message: $0) }
-            label.handleCustomTap(for: customType2) { self.alert("Custom type", message: $0) }
+            label.handleCustomTap(for: customType) { self.alert(title: "Custom type", message: $0) }
+            label.handleCustomTap(for: customType2) { self.alert(title: "Custom type", message: $0) }
         }
 
         label.frame = CGRect(x: 20, y: 40, width: view.frame.width - 40, height: 300)
         view.addSubview(label)
         
-        uiLabel.frame = CGRect(x: 20, y: 350, width: view.frame.width - 40, height: 300)
-        view.addSubview(uiLabel)
+        toggleButton.frame.size = CGSize(width: 200, height: 50)
+        toggleButton.center = CGPoint(x: view.bounds.midX, y: view.bounds.maxY - 50)
+        view.addSubview(toggleButton)
+        toggleButton.addTarget(self, action: #selector(toggleLabelText), for: .touchUpInside)
         
-        // Do any additional setup after loading the view, typically from a nib.
+        toggleLabelText()
     }
 
     override func didReceiveMemoryWarning() {
@@ -88,10 +71,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @objc private func toggleLabelText() {
+        usingAttributedText = !usingAttributedText
+    
+        if usingAttributedText {
+            
+            toggleButton.setTitle("Use Text", for: .normal)
+            
+            let text = NSMutableAttributedString()
+            let s1 = NSAttributedString(string: "BIG AND FAT ", attributes: [
+                NSForegroundColorAttributeName : UIColor.orange,
+                NSFontAttributeName: UIFont.preferredFont(forTextStyle: UIFontTextStyle.headline).withSize(30)
+                ])
+            let s2 = NSAttributedString(string: defaultText, attributes: [
+                NSForegroundColorAttributeName : UIColor(red: 102.0/255, green: 117.0/255, blue: 127.0/255, alpha: 1),
+                NSFontAttributeName: UIFont.preferredFont(forTextStyle: UIFontTextStyle.body)
+                ])
+            text.append(s1)
+            text.append(s2)
+            label.attributedText = text
+            
+        } else {
+            label.text = defaultText
+            
+            toggleButton.setTitle("Use Attributed Text", for: .normal)
+        }
+    }
+    
+    
     func alert(title: String, message: String) {
-        let vc = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
-        vc.addAction(UIAlertAction(title: "Ok", style: .Cancel, handler: nil))
-        presentViewController(vc, animated: true, completion: nil)
+        let vc = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        vc.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+        present(vc, animated: true, completion: nil)
     }
 
 }
