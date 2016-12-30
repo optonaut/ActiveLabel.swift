e91d13dd48a90c106759cfaec8ac0405f684a808
Andrea Perizzato <andrea.perizzato@gmail.com>
2016-12-30 22:10:08 +0100

    Cleaned up AttributedTextTests

--- ActiveLabelTests/AttributedTextTests.swift
+++ ActiveLabelTests/AttributedTextTests.swift
@@ -10,8 +10,6 @@ import Foundation
 import XCTest
 @testable import ActiveLabel
 
-
-
 class AttributedTextTests: XCTestCase {
     
     let label = ActiveLabel()
@@ -44,12 +42,6 @@ class AttributedTextTests: XCTestCase {
     override func setUp() {
         super.setUp()
         label.enabledTypes = [.mention, .hashtag, .url]
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
     }
     
     func testWithoutAttributedString() {
