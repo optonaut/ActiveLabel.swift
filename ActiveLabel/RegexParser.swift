b7786be83e4fbac5ac2d11449a4dfe99c318d1ff
Jonah Siegle <jonah@metalab.co>
2017-09-05 12:39:08 -0600

    Added support for '-' characters in mentions

--- ActiveLabel/RegexParser.swift
+++ ActiveLabel/RegexParser.swift
@@ -11,7 +11,7 @@ import Foundation
 struct RegexParser {
 
     static let hashtagPattern = "(?:^|\\s|$)#[\\p{L}0-9_]*"
-    static let mentionPattern = "(?:^|\\s|$|[.])@[\\p{L}0-9_]*"
+    static let mentionPattern = "(?:^|\\s|$|[.])@[\\p{L}0-9_-]*"
     static let urlPattern = "(^|[\\s.:;?\\-\\]<\\(])" +
         "((https?://|www\\.|pic\\.)[-\\w;/?:@&=+$\\|\\_.!~*\\|'()\\[\\]%#,☺]+[\\w/#](\\(\\))?)" +
     "(?=$|[\\s',\\|\\(\\).:;?\\-\\[\\]>\\)])"
