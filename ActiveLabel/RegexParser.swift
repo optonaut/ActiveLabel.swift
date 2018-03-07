e824102ecb2fa69aab022a0394ec07aa8bd504a7
siengnonta <siengnontapat@gmail.com>
2018-03-07 12:30:10 +0700

    Supporting Thai language

--- ActiveLabel/RegexParser.swift
+++ ActiveLabel/RegexParser.swift
@@ -10,8 +10,8 @@ import Foundation
 
 struct RegexParser {
 
-    static let hashtagPattern = "(?:^|\\s|$)#[\\p{L}0-9_]*"
-    static let mentionPattern = "(?:^|\\s|$|[.])@[\\p{L}0-9_]*"
+    static let hashtagPattern = "(?:^|\\s|$)#[\\p{L}0-9_ก-๙]*"
+    static let mentionPattern = "(?:^|\\s|$|[.])@[\\p{L}0-9_ก-๙]*"
     static let urlPattern = "(^|[\\s.:;?\\-\\]<\\(])" +
         "((https?://|www\\.|pic\\.)[-\\w;/?:@&=+$\\|\\_.!~*\\|'()\\[\\]%#,☺]+[\\w/#](\\(\\))?)" +
     "(?=$|[\\s',\\|\\(\\).:;?\\-\\[\\]>\\)])"
