289ae76aa037e05ac608f643dd54377e10bfdeca
Jacob Clayden <jacie950@gmail.com>
2019-11-18 14:56:45 +0000

    Updated mention and hashtag regex

--- ActiveLabel/RegexParser.swift
+++ ActiveLabel/RegexParser.swift
@@ -10,8 +10,8 @@ import Foundation
 
 struct RegexParser {
 
-    static let hashtagPattern = "(?:^|\\s|$)#[\\p{L}0-9_]*"
-    static let mentionPattern = "(?:^|\\s|$|[.])@[\\p{L}0-9_]*"
+    static let hashtagPattern = "(?<=^|\\s|$|\\.)#[\\p{L}\\d_]*"
+    static let mentionPattern = "(?<=^|\\s|$|\\.)@[\\p{L}\\d_]*"
     static let urlPattern = "(^|[\\s.:;?\\-\\]<\\(])" +
         "((https?://|www\\.|pic\\.)[-\\w;/?:@&=+$\\|\\_.!~*\\|'()\\[\\]%#,☺]+[\\w/#](\\(\\))?)" +
     "(?=$|[\\s',\\|\\(\\).:;?\\-\\[\\]>\\)])"
