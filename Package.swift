5fdf72a046a7b18c65ac1e64da4d61d8a14e8077
Fumiya Sakai <just1factory@gmail.com>
2021-05-06 10:35:15 +0900

    add Package.swift

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ActiveLabel",
+    platforms: [.iOS(.v10)],
+    products: [
+        .library(
+            name: "ActiveLabel",
+            targets: ["ActiveLabel"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "ActiveLabel",
+            path: "ActiveLabel",
+            exclude: ["ActiveLabelDemo"]),
+        .testTarget(
+            name: "ActiveLabelTests",
+            path: "ActiveLabelTests"
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
+)
